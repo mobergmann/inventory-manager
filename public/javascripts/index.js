aab772891b6bf7906c68a2237ed68f84afcdac14
mobergmann <mobergmann@pm.me>
2022-06-06 20:58:11 +0200

    sign up possible

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,54 +1,57 @@
+function extract_user() {
+    let name = document.getElementById("sign-up_username").value;
+    let mail = document.getElementById("sign-up_mail").value;
+    let password = document.getElementById("sign-up_password").value;
+
+    return {
+        name: name,
+        mail: mail,
+        password: password
+    };
+}
+
+
 function sign_in() {
-    const xhr = new XMLHttpRequest();
 
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            let project = JSON.parse(xhr.responseText);
-            display_projects(project);
-        } else if (xhr.status === 404) {
-            display_no_projects_found();
-        }
-    }
-
-    xhr.onerror = function() {
-        alert("Network error occurred");
-    }
-
-    xhr.open('GET', "/api/projects");
-    // xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send();
 }
 
 function sign_up() {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            let project = JSON.parse(xhr.responseText);
-            display_projects(project);
-        } else if (xhr.status === 404) {
-            display_no_projects_found();
-        }
-    }
-
-    xhr.onerror = function() {
-        alert("Network error occurred");
-    }
-
-    xhr.open('POST', "/auth/projects");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send();
+    alert("Sign Up successfully, now you need to Sign In.");
 }
 
 function sign_out() {
+
+}
+
+
+// function _sign_in() {
+//     const xhr = new XMLHttpRequest();
+//
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             sign_in();
+//         } else if (xhr.status === 404) {
+//             alert("404");
+//         }
+//     }
+//
+//     xhr.onerror = function() {
+//         alert("Network error occurred");
+//     }
+//
+//     xhr.open('GET', "/auth/sign_in");
+//     // xhr.setRequestHeader("Content-Type", "application/json");
+//     xhr.send();
+// }
+
+function _sign_up() {
     const xhr = new XMLHttpRequest();
 
     xhr.onload = function() {
         if (xhr.status === 200) {
-            let project = JSON.parse(xhr.responseText);
-            display_projects(project);
+            sign_up();
         } else if (xhr.status === 404) {
-            display_no_projects_found();
+            alert("404");
         }
     }
 
@@ -56,7 +59,27 @@ function sign_out() {
         alert("Network error occurred");
     }
 
-    xhr.open('GET', "/api/projects");
-    // xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send();
+    xhr.open('POST', "/auth/sign_up");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(extract_user()));
 }
+
+// function _sign_out() {
+//     const xhr = new XMLHttpRequest();
+//
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             sign_out();
+//         } else if (xhr.status === 404) {
+//             alert("404");
+//         }
+//     }
+//
+//     xhr.onerror = function() {
+//         alert("Network error occurred");
+//     }
+//
+//     xhr.open('GET', "/auth/sign_out");
+//     // xhr.setRequestHeader("Content-Type", "application/json");
+//     xhr.send();
+// }
